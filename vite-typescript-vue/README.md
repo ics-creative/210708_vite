0050fa0cffbb60b660a5a2959d31f5b93db8c5a4
ics-matsumoto <matsumoto@ics-web.jp>
2021-07-05 19:10:10 +0900

    サンプルからソースへのリンクを追加

--- vite-typescript-vue/README.md
+++
@@ -1,1 +0,0 @@
-# Vite + TypeScript + Vue（+ VueRouter & Vuex）最小サンプル
--- vite-typescript-vue/src/App.vue
+++ vite-typescript-vue/src/App.vue
@@ -3,7 +3,7 @@
     <h1>Vite + TypeScript + Vueサンプル</h1>
     <p>
       Vite + TypeScript + Vue（+ VueRouter & Vuex）のサンプルです。 Viteの標準テンプレートでvue-ts（Vue +
-      TypeScript）を選択した上で、 VueRouterとVuexを後から追加しています。
+      TypeScript）を選択した上で、 VueRouterとVuexを後から追加しています。<a href="https://github.com/ics-creative/210708_vite/tree/main/vite-typescript-vue">ソースコード</a>
     </p>
     <p>
       このサンプルではVueRouterを使ってHomeとAboutの1ページを切り替え、
